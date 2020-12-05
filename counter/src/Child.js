0cf476713582caa75fd7aff1ca242ffdf1bc703b
Pablo <pablo@pom.es>
2020-12-05 13:41:17 +0100

    finish

--- counter/src/Child.js
+++ counter/src/Child.js
@@ -1,13 +1,20 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Child = (props) => {
     console.log(props);
        return (
-        <main className="main">
-            <button onClick={props.increase}>Increment</button>
-            <span>{props.value}</span>
-            <button onClick={props.decrease}>Decrement</button>
-        </main>
+        <div className="row">     
+            <div className="col-md-1">
+                <button onClick={props.increase}>Increment</button>
+            </div>
+            <div className="col-md-1">
+                <span>{props.value}</span>
+            </div>
+            <div className="col-md-1">
+                <button onClick={props.decrease}>Decrement</button>
+            </div>
+        </div>    
        );
     }
 
