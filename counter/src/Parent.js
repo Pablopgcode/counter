74ada24393f098097ada5e76e08e93cd3b7e2aa6
Pablo Pardo <Pablo@MacBook-Pro-de-admin.local>
2020-12-08 07:41:00 +0100

    modify

--- counter/src/Parent.js
+++ counter/src/Parent.js
@@ -2,25 +2,53 @@ import React from "react";
 import Child from './Child';
 
 class Parent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state =  {count : 0}                                         
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state =  {count : 0}                                         
+    // }
 
-    increment() {
-        this.setState({count: parseInt(this.state.count + 1)});
-    }
+    state =  {
+        count : 0
+    }    
 
-    decrement() {
-        if (this.state.count > 0) {
-            this.setState({count: parseInt(this.state.count -1)});
+    // increment() {
+    //     this.setState({count: parseInt(this.state.count + 1)});
+    // }
+
+    // decrement() {
+    //     if (this.state.count > 0) {
+    //         this.setState({count: parseInt(this.state.count -1)});
+    //     }
+    // }
+
+    increment = () => {
+        const { count } = this.state   //abreviamos this.state.count a count (codigo más limpio)
+        this.setState({
+            count: count + 1
+        });
+    }
+    decrement = () => {
+        const { count } = this.state
+        if (count > 0) {
+            this.setState({
+                count: count - 1
+            });
         }
     }
 
+    // render() {
+    //    return (
+    //     <main className="main">
+    //         <Child increase={this.increment.bind(this)} decrease={this.decrement.bind(this)} value={this.state.count} />    
+    //     </main>
+    //    );
+    // } 
+    
     render() {
+        const { count } = this.state
        return (
         <main className="main">
-            <Child increase={this.increment.bind(this)} decrease={this.decrement.bind(this)} value={this.state.count} />    
+            <Child increase={this.increment} decrease={this.decrement} value={count} />    
         </main>
        );
     }     
